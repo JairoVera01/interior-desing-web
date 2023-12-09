7114c92665526eb52d6ddc9e9f324281e9a16fe5
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-12-09 13:27:11 -0500

    Commit

--- kursor.d.ts
+++ kursor.d.ts
@@ -1,5 +1,9 @@
 declare module "kursor" {
   export default class Kursor {
-    constructor(options: { type: number; removeDefaultCursor?: boolean });
+    constructor(options: {
+      type: number;
+      removeDefaultCursor?: boolean;
+      el?: string;
+    });
   }
 }
